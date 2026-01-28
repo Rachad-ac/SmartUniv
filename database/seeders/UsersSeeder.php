67d8a2c01cceb7b09b5e41a2f7f2f9b93389b3b6
Rachad <161521354+Rachad-ac@users.noreply.github.com>
2026-01-28 16:58:58 +0000

    Update user emails in UsersSeeder

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -11,11 +11,10 @@ class UsersSeeder extends Seeder
     public function run()
     {
         $users = [
-            ['nom'=>'Rachad','prenom'=>'Ahmed Combo','email'=>'bent35005@gmail.com','password'=>Hash::make('password'),'role_id'=>1],
-            ['nom'=>'Fall','prenom'=>'Moussa','email'=>'enseignant1@test.com','password'=>Hash::make('password'),'role_id'=>2],
-            ['nom'=>'Diallo','prenom'=>'Awa','email'=>'etudiant1@test.com','password'=>Hash::make('password'),'role_id'=>3],
-            ['nom'=>'Seck','prenom'=>'Fatou','email'=>'technicien@test.com','password'=>Hash::make('password'),'role_id'=>4],
-            ['nom'=>'Mbaye','prenom'=>'Ali','email'=>'guest@test.com','password'=>Hash::make('password'),'role_id'=>5],
+            ['nom'=>'Rachad','prenom'=>'Ahmed Combo','email'=>'bent35005@gmail.com','password'=>Hash::make('password'),'role_id'=>1], //mail reel de messagerie admin
+            ['nom'=>'Fall','prenom'=>'Moussa','email'=>'teacher@smartuniv.com','password'=>Hash::make('password'),'role_id'=>2],
+            ['nom'=>'Diallo','prenom'=>'Awa','email'=>'student@smartuniv.com','password'=>Hash::make('password'),'role_id'=>3],
+            ['nom'=>'Seck','prenom'=>'Fatou','email'=>'admin@smartuniv.com','password'=>Hash::make('password'),'role_id'=>1],
         ];
 
         DB::table('users')->insert($users);
